1b93d81ef16d2d8c6de0f0cdc61830d4030fd71c
Julia Blaustein <jublaustein@icloud.com>
2023-07-17 10:07:31 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    // screens: {
+    //   'sm': {'min': '280px', 'max': '765px'}
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
